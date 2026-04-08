39c396a796e4d606d809062abc16930f4bcf31ac
Rad586 <99306685+Rad586@users.noreply.github.com>
2026-04-08 20:01:36 +0800

    new sfx

--- kubejs/startup_scripts/2_registry/sound_events.js
+++ kubejs/startup_scripts/2_registry/sound_events.js
@@ -1,6 +1,6 @@
 const custom_sounds = [
     "whirlwind", "smite", "arc", 
     "blizzard", "inferno", "vortex", 
-    "lunge"
+    "lunge", "destroy_projectile"
 ]
 StartupEvents.registry("sound_event", e => custom_sounds.forEach(s => e.create("fmn:" + s)))
