573d074ae3817bc4b0c3f6170f4c9a7438791e28
Rad586 <99306685+Rad586@users.noreply.github.com>
2026-04-09 11:33:37 +0800

    look better

--- kubejs/client_scripts/sided/sit_client.js
+++ kubejs/client_scripts/sided/sit_client.js
@@ -1,22 +1,10 @@
-let last_pressed = 0;
-let holding = false;
-
 function sitClient(player) {
-    if (player.pitch < 60 || player.isSprinting() ||
-        !player.isOnGround() || !player.feetBlockState.fluidState.isEmpty()
+    if (player.pitch < 60 || 
+        player.isSprinting() ||
+        !player.isOnGround() || 
+        !player.feetBlockState.fluidState.isEmpty() ||
+        !pressedTwice("key_sit")
     ) return;
 
-    // const { keyShift } = Client.options;
-    // const pressing = keyShift.isDown();
-
-    // if (pressing && !holding) {
-    //     if (Date.now() - last_pressed <= 300) {
-    //         player.sendData("sit");
-    //         last_pressed = 0
-    //     } 
-    //     else last_pressed = Date.now()
-    // };
-
-    // holding = pressing
-    if (pressedTwice("key_sit")) player.sendData("sit")
+    player.sendData("sit")
 }
