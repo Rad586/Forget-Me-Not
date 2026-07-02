eadfd24a696dcdfb0c0e14fd202ed130a4576ce0
Rad586 <99306685+Rad586@users.noreply.github.com>
2026-07-02 19:16:34 +0800

    well, you can always learn something new

--- kubejs/server_scripts/2_modules/mechanics/hotbar_totem.js
+++ kubejs/server_scripts/2_modules/mechanics/hotbar_totem.js
@@ -1,13 +1,13 @@
 function hotbar_totem(e, level, player) {
     const { inventory: inv } = player;
-    const slot = inv.find("totem_of_undying")
-    if ([null, 40, player.selectedSlot].includes(slot) || slot > 8) return;
+    const slot = inv.find("minecraft:totem_of_undying");
+    if ([-1, 40, player.selectedSlot].includes(slot) || slot > 8) return;
 
     const { potionEffects } = player;
     player.setHealth(1.0);
-    potionEffects.add("regeneration", 900, 1);
-    potionEffects.add("fire_resistance", 800, 0);
-    potionEffects.add("absorption", 100, 1);
+    potionEffects.add("regeneration", 900, 1, false, true);
+    potionEffects.add("fire_resistance", 800, 0, false, true);
+    potionEffects.add("absorption", 100, 1, false, true);
     inv.getStackInSlot(slot).shrink(1);
 
     level.broadcastEntityEvent(player, 35);
