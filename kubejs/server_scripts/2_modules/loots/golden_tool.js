84446ca21d1e7e656686a756e56d9202aca77bee
Rad586 <99306685+Rad586@users.noreply.github.com>
2026-07-13 21:07:02 +0800

    anti loop

--- kubejs/server_scripts/2_modules/loots/golden_tool.js
+++ kubejs/server_scripts/2_modules/loots/golden_tool.js
@@ -1,16 +1,24 @@
+let processing_bl = false
+
 function golden_tool(e) {
     e.addLootTypeModifier(LootType.BLOCK)
         .matchMainHand([
             "minecraft:golden_pickaxe", "minecraft:golden_axe",
             "minecraft:golden_shovel", "minecraft:golden_hoe"
         ])
+        .playerPredicate(p => !!p && !processing_bl)
         .apply(c => {
+            processing_bl = true;
+
             c.removeLoot(ItemFilter.ALWAYS_TRUE);
             global.getBlockLoot(
                 c.level,
+                c.player,
                 c.tool.enchant("silk_touch", 1),
                 c.getDestroyedBlock()
-            ).forEach(s => c.addLoot(s))
+            ).forEach(s => c.addLoot(s));
+
+            processing_bl = false
         })
         .functions("#c:ores", s => s.smeltLoot())
 }
