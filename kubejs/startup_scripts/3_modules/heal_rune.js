b2100ee9e51fde424744693978e99c4e51254047
Rad586 <99306685+Rad586@users.noreply.github.com>
2026-07-16 20:24:10 +0800

    fix heal rune

--- kubejs/startup_scripts/3_modules/heal_rune.js
+++ kubejs/startup_scripts/3_modules/heal_rune.js
@@ -3,7 +3,7 @@ function heal_rune(context) {
     const { level } = entity;
 
     if (level.isClientSide()) return;
-    global.trinkets["bless"](
+    global.trinkets["bless"].action(
         level, entity, null,
         global.trinketAmount(entity, "bless"),
         context.healAmount
